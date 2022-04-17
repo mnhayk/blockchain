c4f571209a762f14bd1fcaf283c6ab50950b6b13
Hayk Mnatsakanyan <hayk.mnatsakanyan@thetrainline.com>
2022-04-17 17:16:49 +0400

    Clean up

--- contracts/FootballLeagueTokens.sol
+++ contracts/FootballLeagueTokens.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FootballLeagueTokens is ERC1155, Ownable  {
 
