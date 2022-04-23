e4950e8b045738b32612d0db7eedd7d5fa38b95d
Hayk Mnatsakanyan <hayk.mnatsakanyan@thetrainline.com>
2022-04-23 23:25:54 +0400

    Changed config files and added verification library.

--- truffle-config.js
+++ truffle-config.js
@@ -28,6 +28,7 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 const MNEMONIC = process.env.MNEMONIC
 const ALCHEMY = process.env.ALCHEMY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
   /**
@@ -39,6 +40,7 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
+   plugins: ['truffle-plugin-verify'],
    networks: {
     development: {
       host: '127.0.0.1', // Localhost (default: none)
@@ -65,6 +67,10 @@ module.exports = {
     //   provider: () => new HDWalletProvider(mnemonic, alchemyURL),
     //   network_id: 137,
     // },
+  },
+
+  api_keys: {
+    etherscan: ETHERSCAN_API_KEY,
   },
 
   // Set default mocha options here, use special reporters etc.
