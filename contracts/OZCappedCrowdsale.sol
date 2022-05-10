4173eaede5456906205ec61b20d7f3ddf659ab38
Hayk Mnatsakanyan <hayk.mnatsakanyan@thetrainline.com>
2022-05-10 11:58:05 +0400

    Added OZCrowdsale.sol and OZCappedCrowdsale.sol contracts

---
+++ contracts/OZCappedCrowdsale.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.10 and less than 0.9.0
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./OZCrowdsale.sol";
+
+/**
+ * @title CappedCrowdsale
+ * @dev Crowdsale with a limit for total contributions.
+ */
+contract CappedCrowdsale is OZCrowdsale {
+    using SafeMath for uint256;
+
+    uint256 private _cap;
+
+    /**
+     * @dev Constructor, takes maximum amount of wei accepted in the crowdsale.
+     * @param cap Max amount of wei to be contributed
+     */
+    constructor (uint256 cap, uint256 rate, address payable wallet, IERC20 token) OZCrowdsale(rate, wallet, token) {
+        require(cap > 0, "CappedCrowdsale: cap is 0");
+        _cap = cap;
+    }
+
+    /**
+     * @return the cap of the crowdsale.
+     */
+    function getCap() public view returns (uint256) {
+        return _cap;
+    }
+
+    /**
+     * @dev Checks whether the cap has been reached.
+     * @return Whether the cap was reached
+     */
+    function capReached() public view returns (bool) {
+        return weiRaised() >= _cap;
+    }
+
+    /**
+     * @dev Extend parent behavior requiring purchase to respect the funding cap.
+     * @param beneficiary Token purchaser
+     * @param weiAmount Amount of wei contributed
+     */
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view override {
+        super._preValidatePurchase(beneficiary, weiAmount);
+        require(weiRaised().add(weiAmount) <= _cap, "CappedCrowdsale: cap exceeded");
+    }
+}
