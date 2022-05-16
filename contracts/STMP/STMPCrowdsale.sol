a50517363e3c10e437069431bd312a89d9933bb6
Hayk Mnatsakanyan <hayk.mnatsakanyan@thetrainline.com>
2022-05-16 23:42:13 +0400

    Refactored and improved logic.

--- contracts/STMP/STMPCrowdsale.sol
+++ contracts/STMP/STMPCrowdsale.sol
@@ -9,23 +9,27 @@ import "./TimedCrowdsale.sol";
 
 contract STMPCrowdsale is Crowdsale, TimedCrowdsale, Ownable {
     uint256 public fullTokenAmount;
-    uint256 public icoTokenAmount;
     address public tokenAddress;
+
+    //TODO: Rate should be calculated
+    uint256 public ratePreSale = 6000;
 
     uint256 public rateOne = 5000;
     uint256 public rateTwo = 4000;
     uint256 public rateThree = 3000;
     uint256 public rateFour = 2000;
-    uint256 public limitTierOne = 150e6 * (10**(STMPToken(address(getToken())).decimals()));
-    uint256 public limitTierTwo = 150e6 * (10**(STMPToken(address(getToken())).decimals()));
-    uint256 public limitTierThree = 150e6 * (10**(STMPToken(address(getToken())).decimals()));
-    uint256 public limitTierFour = 350e6 * (10**(STMPToken(address(getToken())).decimals()));
 
-    // Crowdsale Stages
-    // ICOFirstStage => 150m at $ 0.03 ($4.5m),
-    // ICOSecondStage => 150m at $ 0.04 ($6m)
-    // ICOThirdStage => 150m at $ 0.05 ($7.5m)
-    // ICOForthStage => 350m plus any pre-sale unsold at price of $ 0.08 ($28m)
+    uint256 public stagePreSaleLimit =
+        3e6 * (10**(STMPToken(address(getToken())).decimals()));
+
+    uint256 public stageOneLimit =
+        153e6 * (10**(STMPToken(address(getToken())).decimals()));
+    uint256 public stageTwoLimit =
+        303e6 * (10**(STMPToken(address(getToken())).decimals()));
+    uint256 public stageThreeLimit =
+        453e6 * (10**(STMPToken(address(getToken())).decimals()));
+    uint256 public stageFourLimit =
+        803e6 * (10**(STMPToken(address(getToken())).decimals()));
 
     enum CrowdsaleStage {
         PreSale,
@@ -37,7 +41,6 @@ contract STMPCrowdsale is Crowdsale, TimedCrowdsale, Ownable {
 
     constructor(
         uint256 _fullTokenAmount,
-        uint256 _icoTokenAmount,
         uint256 _openingTime,
         uint256 _closingTime,
         uint256 _tokenRate,
@@ -47,15 +50,9 @@ contract STMPCrowdsale is Crowdsale, TimedCrowdsale, Ownable {
         Crowdsale(_tokenRate, _wallet, IERC20(_tokenAddress))
         TimedCrowdsale(_openingTime, _closingTime)
     {
-        require(
-            _fullTokenAmount > 0 &&
-                _icoTokenAmount > 0 &&
-                _icoTokenAmount <= _fullTokenAmount,
-            "Invalid Token amount"
-        );
+        require(_fullTokenAmount > 0, "Invalid Token amount");
         require(_tokenAddress != address(0), "Invalid address");
         fullTokenAmount = _fullTokenAmount;
-        icoTokenAmount = _icoTokenAmount;
         tokenAddress = _tokenAddress;
 
         STMPToken(tokenAddress).mint(address(this), fullTokenAmount);
@@ -65,7 +62,6 @@ contract STMPCrowdsale is Crowdsale, TimedCrowdsale, Ownable {
      * @dev Allows admin to update the crowdsale stage
      * @param _stage Crowdsale stage
      */
-    //TODO: Rate should be calculated
     function setCrowdsaleStage(uint256 _stage) public onlyOwner {
         if (uint256(CrowdsaleStage.PreSale) == _stage) {
             stage = CrowdsaleStage.PreSale;
@@ -74,50 +70,35 @@ contract STMPCrowdsale is Crowdsale, TimedCrowdsale, Ownable {
         }
     }
 
-    function _getTokenAmount(uint256 weiAmount)
-        internal
-        view
-        override
-        returns (uint256)
-    {
+    function _getTokenAmount(uint256 weiAmount) internal view override returns (uint256) {
+
         uint256 tokensToBuy;
-        uint256 rate;
-        uint256 tierSelected;
-        uint256 limitTier;
-        if (tokenRaised() <= limitTierOne) {
-            rate = rateOne;
-            limitTier = limitTierOne;
-            tierSelected = 1;
-        } else if (
-            tokenRaised() > limitTierOne && tokenRaised() <= limitTierTwo
-        ) {
-            rate = rateTwo;
-            limitTier = limitTierTwo;
-            tierSelected = 2;
-        } else if (
-            tokenRaised() > limitTierTwo && tokenRaised() <= limitTierThree
-        ) {
-            rate = rateThree;
-            limitTier = limitTierThree;
-            tierSelected = 3;
-        } else if (
-            tokenRaised() > limitTierThree && tokenRaised() <= limitTierFour
-        ) {
-            rate = rateFour;
-            limitTier = limitTierFour;
-            tierSelected = 4;
-        }
+        uint256 decimals = STMPToken(address(getToken())).decimals();
 
-        tokensToBuy = ((weiAmount * (10**(STMPToken(address(getToken())).decimals()) / 1 ether))) * rate;
-
-        // If the amount of tokens that you want to buy gets out of this tier
-        if (tokenRaised() + tokensToBuy > limitTier) {
-            tokensToBuy = calculateExcessTokens(
-                weiAmount,
-                limitTier,
-                tierSelected,
-                rate
-            );
+        if (stage == CrowdsaleStage.PreSale) {
+            tokensToBuy = ((weiAmount * (10**decimals)) / 1 ether) * ratePreSale;
+            if (tokenRaised() + tokensToBuy > stagePreSaleLimit) {
+                tokensToBuy = calculateExcessTokens(weiAmount, stagePreSaleLimit, 0, ratePreSale);
+             }
+        } else if (stage == CrowdsaleStage.ICO) {
+            if (tokenRaised() < stageOneLimit) {
+                tokensToBuy = ((weiAmount * (10**decimals)) / 1 ether) * rateOne;
+                if (tokenRaised() + tokensToBuy > stageOneLimit) {
+                    tokensToBuy = calculateExcessTokens(weiAmount, stageOneLimit, 1, rateOne);
+                }
+            } else if (tokenRaised() >= stageOneLimit && tokenRaised() < stageTwoLimit) {
+                tokensToBuy = ((weiAmount * (10**decimals)) / 1 ether) * rateTwo;
+                if (tokenRaised() + tokensToBuy > stageTwoLimit) {
+                    tokensToBuy = calculateExcessTokens(weiAmount, stageTwoLimit, 2, rateTwo);
+                }
+            } else if (tokenRaised() >= stageTwoLimit && tokenRaised() < stageThreeLimit) {
+                tokensToBuy = ((weiAmount * (10**decimals)) / 1 ether) * rateThree;
+                if (tokenRaised() + tokensToBuy > stageThreeLimit) {
+                    tokensToBuy = calculateExcessTokens(weiAmount, stageThreeLimit, 3, rateThree);
+                }
+            } else if (tokenRaised() >= stageThreeLimit) {
+                tokensToBuy = ((weiAmount * (10**decimals)) / 1 ether) * rateFour;
+            }
         }
         return tokensToBuy;
     }
@@ -135,50 +116,46 @@ contract STMPCrowdsale is Crowdsale, TimedCrowdsale, Ownable {
     }
 
     //Private functions
-
     function calculateExcessTokens(
         uint256 amount,
-        uint256 tokensThisTier,
-        uint256 tierSelected,
+        uint256 currentStageTokens,
+        uint256 currentStage,
         uint256 _rate
-    ) private view returns (uint256 totalTokens) {
-        require(amount > 0 && tokensThisTier > 0 && _rate > 0);
-        require(tierSelected >= 1 && tierSelected <= 4);
-
-        uint256 weiThisTier = tokensThisTier - (tokenRaised()) / (_rate);
-        uint256 weiNextTier = amount - (weiThisTier);
-        uint256 tokensNextTier = 0;
+    ) internal view returns (uint256 totalTokens) {
+        uint256 currentStageWei = (currentStageTokens - tokenRaised()) / _rate;
+        uint256 nextStageWei = amount - currentStageWei;
+        uint256 nextStageTokens = 0;
         bool returnTokens = false;
 
-        // If there's excessive wei for the last tier, refund those
-        if (tierSelected != 4)
-            tokensNextTier = calculateTokensTier(
-                weiNextTier,
-                tierSelected + (1)
-            );
-        else returnTokens = true;
-
-        totalTokens = tokensThisTier - tokenRaised() + (tokensNextTier);
+        if (currentStage != 4 || currentStage != 0) {
+            nextStageTokens = calculateStageTokens(nextStageWei, currentStage + 1);
+        } else {
+            returnTokens = true;
+        }
+        totalTokens = currentStageTokens - tokenRaised() + nextStageTokens;
 
         // Do the transfer at the end
-        //TODO: should be checked
-        // if (returnTokens) { 
-        //     (bool success, ) = msg.sender.call {value: weiNextTier }("");
-        //     require(success, "refunding failed");
-        // }
+        // if(returnTokens) msg.sender.transfer(weiNextTier);
     }
 
-    function calculateTokensTier(uint256 weiPaid, uint256 tierSelected)
-        private
+    function calculateStageTokens(uint256 weiPaid, uint256 currentStage)
+        internal
         view
         returns (uint256 calculatedTokens)
     {
         require(weiPaid > 0);
-        require(tierSelected >= 1 && tierSelected <= 4);
-
-        if (tierSelected == 1) calculatedTokens = weiPaid * rateOne;
-        else if (tierSelected == 2) calculatedTokens = weiPaid * rateTwo;
-        else if (tierSelected == 3) calculatedTokens = weiPaid * rateThree;
-        else calculatedTokens = weiPaid * rateFour;
+        require(currentStage >= 0 && currentStage <= 4);
+        
+        if (currentStage == 0) {
+            calculatedTokens = weiPaid * ratePreSale;
+        } else if (currentStage == 1) {
+            calculatedTokens = weiPaid * rateOne;
+        } else if (currentStage == 2) {
+            calculatedTokens = weiPaid * rateTwo;
+        } else if (currentStage == 3) {
+            calculatedTokens = weiPaid * rateThree;
+        } else {
+            calculatedTokens = weiPaid * rateFour;
+        }
     }
 }
