af436570efc320268b7e57fd55c1348a941b00f9
Hayk Mnatsakanyan <hayk.mnatsakanyan@thetrainline.com>
2022-06-21 15:59:51 +0200

    Test commit

--- contracts/UniqueAsset.sol
+++ contracts/UniqueAsset.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
